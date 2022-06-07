3353a3adea79a9d8bd4bcb40b3b2a6ed9928cd80
Tamilselvan Velayudham <75055998+Tamilselvan-dev@users.noreply.github.com>
2022-06-07 20:19:55 +0530

    night-vision-commit

--- src/Components/EarthComponent/Earth.js
+++ src/Components/EarthComponent/Earth.js
@@ -1,20 +1,39 @@
-import React from "react";
-import { useLoader } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import earth from "../../../public/world.jpg";
 import cloud from "../../../public/clouds.jpg";
+import negativeMap from "../../../public/earth-negative-map.jpg";
+import nightMap from "../../../public/earth-night-map.jpg";
+import terrainMap from "../../../public/earth-terrain-map.jpg";
 import { DoubleSide, TextureLoader } from "three";
 import { OrbitControls, Stars } from "@react-three/drei";
 
 const Earth = () => {
-  const [colorMap, cloudMap] = useLoader(TextureLoader, [earth, cloud]);
+  const [
+    colorMap,
+    cloudMap,
+    NightMap,
+    TerrainMap,
+    NegativeMap
+  ] = useLoader(TextureLoader, [
+    earth,
+    cloud,
+    nightMap,
+    terrainMap,
+    negativeMap
+  ]);
+
+  const earthRef = useRef();
 
   return (
     <>
-      <ambientLight intensity={1.5} />
-      <Stars radius={300} depth={60} count={20000} factor={7} fade={true} />
+      {/* <ambientLight intensity={2} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={2} />
+
+      <Stars radius={300} depth={60} count={8000} factor={7} fade={true} />
 
       <mesh>
-        <sphereGeometry args={[1.5, 60, 60]} />
+        <sphereGeometry args={[2, 60, 60]} />
         <meshPhongMaterial
           map={cloudMap}
           opacity={0.5}
@@ -25,9 +44,14 @@ const Earth = () => {
       </mesh>
 
       <mesh>
-        <sphereGeometry args={[1.5, 60, 60]} />
-        <meshPhongMaterial />
-        <meshStandardMaterial map={colorMap} />
+        <sphereGeometry args={[2, 60, 60]} />
+        <meshPhongMaterial specularMap={NegativeMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={TerrainMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
         <OrbitControls
           enableZoom={true}
           enablePan={true}
